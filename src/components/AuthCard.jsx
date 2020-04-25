f86ea9e8fd2daa73031d90a214652e24dbe3038c
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2020-04-25 15:45:12 -0700

    Fix signin on mobile

--- src/components/AuthCard.jsx
+++ src/components/AuthCard.jsx
@@ -12,7 +12,7 @@ import {
 // Configure FirebaseUI.
 const uiConfig = {
   // Popup signin flow rather than redirect flow.
-  signInFlow: 'popup',
+  signInFlow: 'redirect',
   // Redirect to /signedIn after sign in is successful.
   // Alternatively you can provide a callbacks.signInSuccess function.
   signInSuccessUrl: '/app',
