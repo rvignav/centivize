72040cdd8b8ed75b8d2db3ddc56349af535b7ca0
rvignav <rvignav@gmail.com>
2020-04-25 11:31:03 -0700

    intial post creation

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,14 +1,65 @@
-import React, { Component } from 'react'
-import Footer from "../components/Footer"
+import React, { Component } from 'react';
+import { Tabs } from '@yazanaabed/react-tabs';
+import Box from '@material-ui/core/Box';
 
-export class Home extends Component {
+export default class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: 'Please write an essay about your favorite DOM element.',
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    handleSubmit(event) {
+        alert(`An essay was submitted: ${this.state.value}`);
+        event.preventDefault();
+    }
+
     render() {
         return (
-            <div>
-                <h1>Post page</h1>
+            <div className="card card-body shadow-sm mb-2">
+                <Tabs
+                    activeTab={{
+                        id: 'tab1',
+                    }}
+                >
+                    <Tabs.Tab id="tab1" title="Tab 1">
+                        <div style={{ padding: 10 }}>
+                            <form onSubmit={this.handleSubmit}>
+                                <label>
+                                    Essay:
+                  <textarea
+                                        value={this.state.value}
+                                        onChange={this.handleChange}
+                                    />
+                                </label>
+                                <input type="submit" value="Submit" />
+                            </form>
+                        </div>
+                    </Tabs.Tab>
+                    <Tabs.Tab id="tab2" title="Tab 2">
+                        <div style={{ padding: 10 }}>
+                            <form onSubmit={this.handleSubmit}>
+                                <label>
+                                    Essay:
+                  <textarea
+                                        value={this.state.value}
+                                        onChange={this.handleChange}
+                                    />
+                                </label>
+                                <input type="submit" value="Submit" />
+                            </form>
+                        </div>
+                    </Tabs.Tab>
+                </Tabs>
             </div>
-        )
+        );
     }
 }
-
-export default Home
