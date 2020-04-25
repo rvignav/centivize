f2cb42863bfba0adbd983d4e4c520cf8d10958fc
MasonWang025 <mason.wang0025@gmail.com>
2020-04-25 03:43:54 -0700

    search

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -4,16 +4,28 @@ import Header from "../components/Header"
 
 export class Search extends Component {
     render() {
-        return (
-            <div className="container-fluid">
-                <div className="row pt-3">
-                    <div className="col">
-                        <form>
-                            <input type="text" className="form-control" placeholder="Search..." />
+        return ( 
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <form className="form-inline pt-3">
+                            <div className="flex-fill mr-2">
+                                <input type="search" name="" className="form-control w-100" placeholder="Search..." id=""/>
+                            </div>
+                            <button className="btn btn-primary">Map</button>
                         </form>
                     </div>
                 </div>
             </div>
+            // <div className="container-fluid">
+            //     <div className="row pt-3">
+            //         <div className="col">
+            //             <form>
+            //                 <input type="text" className="form-control" placeholder="Search..." />
+            //             </form>
+            //         </div>
+            //     </div>
+            // </div>
         )
     }
 }
