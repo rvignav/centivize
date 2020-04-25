9758565d3cdf0c78fceb875480eb1dcd9b429d29
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2020-04-25 01:34:28 -0700

    Add router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./Components/App";
+import App from "./App";
 import "./css/main.css"
 
 import 'bootstrap/dist/css/bootstrap.min.css';
