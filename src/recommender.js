fe5a66188a31621c80b1ddb452be4b1b3f0513f2
rvignav <rvignav@gmail.com>
2020-04-26 02:02:52 -0700

    compiles properly

--- src/recommender.js
+++ src/recommender.js
@@ -1,14 +1,15 @@
 const recommend = async () => {
-  const functionEndpoint =
-    'https://us-central1-hack-now-28797.cloudfunctions.net/similarity';
-  fetch(functionEndpoint)
-    .then((res) => res.json())
-    .then(function (data) {
-      console.log(data);
-    })
-    .catch(function (error) {
-      console.log(`Err is:${err}`);
-    });
+  fetch('https://us-central1-hack-now-28797.cloudfunctions.net/similarity', {
+    body: '{"par1":"Hello", "par2":"Goodbye"}',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
+  }.then((response) => {
+      response.json().then(data => {
+        console.log(data);
+      });
+  });
 };
 
 export default recommend;
