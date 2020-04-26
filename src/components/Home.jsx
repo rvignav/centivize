0b543eaeaf0e776d39536d0b24ccec3b96534e13
rvignav <rvignav@gmail.com>
2020-04-25 17:43:55 -0700

    store location in Firestore

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-import Header from "./Header"
-import PostFeed from "./PostFeed"
+import Header from './Header';
+import PostFeed from './PostFeed';
 
-import dummyData from "../DUMMY"
+import dummyData from '../DUMMY';
+import { db } from '../firebase/firebase.utils.js';
 
 export class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    componentDidMount() {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            const { latitude } = position.coords;
+            const { longitude } = position.coords;
+            const user = 'test';
+            db.collection(user)
+                .add({
+                    latitude,
+                    longitude,
+                })
+                .then(function () {
+                    console.log('DONE');
+                });
+        });
+    }
+
     render() {
         const posts = dummyData;
 
@@ -16,8 +38,8 @@ export class Home extends Component {
                     <PostFeed posts={posts} />
                 </div>
             </div>
-        )
+        );
     }
 }
 
-export default Home
+export default Home;
