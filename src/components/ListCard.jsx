2e34158e7cb544fd66998d83217b104cee3cf825
masonwang025 <mason.wang0025@gmail.com>
2020-04-25 18:49:12 -0700

    styles

--- src/components/ListCard.jsx
+++ src/components/ListCard.jsx
@@ -1,31 +1,56 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 export class ListCard extends Component {
-    toggleCollapsed = (id) => {
-        let button = document.getElementById("collapseButton" + id)
-        button.innerText = button.innerText == "Learn more" ? "Show less" : "Learn more"
-        let info = document.getElementById("collapseInfo" + id)
-        info.style.display = info.style.display == "block" ? "none" : "block"
-    }
+  toggleCollapsed = (id) => {
+    let button = document.getElementById('collapseButton' + id);
+    button.innerText =
+      button.innerText == 'Learn more' ? 'Show less' : 'Learn more';
+    let info = document.getElementById('collapseInfo' + id);
+    info.style.display = info.style.display == 'block' ? 'none' : 'block';
+  };
 
-    render() {
-        return (
-            <div key={this.props.post.id} className="card mb-2 m-0 w-100 post">
-                <div className="card-header">
-                    <h4>{this.props.post.title}</h4>
-                    <h5>{this.props.post.name} <small>-{this.props.post.date}</small></h5>
-                </div>
-                <div className="card-body">
-                    <p>{this.props.post.intro}</p>
-                    <button className="btn btn-primary mr-2" >Help {this.props.post.name}</button>
-                    <button id={"collapseButton" + this.props.post.id} className="btn btn-secondary" onClick={() => this.toggleCollapsed(this.props.post.id)}>Learn more</button>
-                    <div style={{ display: "none", paddingTop: "1em" }} id={"collapseInfo" + this.props.post.id}>
-                        {this.props.post.info}
-                    </div>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div key={this.props.post.id} className="card mb-2 m-0 w-100 post">
+        <div className="card-header">
+          <h4>
+            {this.props.post.title}{' '}
+            <FontAwesomeIcon
+              className="text-primary"
+              icon={faStar}
+              style={{
+                display: this.props.post.recommended ? 'inline' : 'none',
+              }}
+            />
+          </h4>
+          <h5>
+            {this.props.post.name} <small>-{this.props.post.date}</small>
+          </h5>
+        </div>
+        <div className="card-body">
+          <p>{this.props.post.intro}</p>
+          <button className="btn btn-primary mr-2">
+            Help {this.props.post.name.split(' ')[0]}
+          </button>
+          <button
+            id={'collapseButton' + this.props.post.id}
+            className="btn btn-secondary"
+            onClick={() => this.toggleCollapsed(this.props.post.id)}
+          >
+            Learn more
+          </button>
+          <div
+            style={{ display: 'none', paddingTop: '1em' }}
+            id={'collapseInfo' + this.props.post.id}
+          >
+            {this.props.post.info}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default ListCard
+export default ListCard;
--- src/dummyposts.js
+++ src/dummyposts.js
@@ -8,6 +8,24 @@ const dummyPosts = [
     info:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore eius soluta voluptatem aliquid eum. Vitae accusamus consectetur cupiditate sed facilis quis reprehenderit quidem illum at. Sed illum sit eveniet laboriosam?',
   },
+  {
+    id: 2,
+    title: 'Need Medicine X',
+    name: 'Jane Doe',
+    date: '12/22/2019',
+    intro:
+      'We cannot access medicine X and I am going through severe pain. Any help is appreciated.',
+    info:
+      'Tempore eius soluta voluptatem aliquid eum. Vitae accusamus consectetur cupiditate sed facilis quis reprehenderit quidem illum at. Sed illum sit eveniet laboriosam?',
+  },
+  {
+    id: 3,
+    title: 'Give potato',
+    name: 'Mason Wang',
+    date: '4/25/2020',
+    intro: 'I want potatoes',
+    info: 'Yes I do',
+  },
 ];
 
 export default dummyPosts;
