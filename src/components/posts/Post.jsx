bae09803d5a274f2216e00521d4f0f3b7f370875
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2020-04-26 00:15:43 -0700

    Use functional

--- src/components/posts/Post.jsx
+++ src/components/posts/Post.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import { Tabs } from '@yazanaabed/react-tabs';
 import { confirmAlert } from 'react-confirm-alert'; // Import
@@ -7,51 +7,34 @@ import diagnose from '../../diagnosis.js';
 import { useUid } from '../../hooks/auth.js';
 import 'react-confirm-alert/src/react-confirm-alert.css';
 
-export default class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: '',
-      text: '',
-      gender: '',
-      year: '',
-      symptoms: '',
-    };
+const Post = () => {
+  const [value, setValue] = useState('');
+  const [text, setText] = useState('');
+  const [gender, setGender] = useState('');
+  const [year, setYear] = useState('');
+  const [symptoms, setSymptoms] = useState('');
 
-    this.componentDidMount() {
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
 
-    }
+  const handleChange5 = (event) => {
+    setSymptoms(event.target.value);
+  };
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleChange2 = this.handleChange2.bind(this);
-    this.handleChange3 = this.handleChange3.bind(this);
-    this.handleChange4 = this.handleChange4.bind(this);
-    this.handleChange5 = this.handleChange5.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleSubmit2 = this.handleSubmit2.bind(this);
-  }
+  const handleChange2 = (event) => {
+    setText(event.target.value);
+  };
 
-  handleChange(event) {
-    this.setState({ value: event.target.value });
-  }
-
-  handleChange5(event) {
-    this.setState({ symptoms: event.target.value });
-  }
-
-  handleChange2(event) {
-    this.setState({ text: event.target.value });
-  }
-
-  handleChange3(event) {
+  const handleChange3 = (event) => {
     this.setState({ gender: event.target.value });
-  }
+  };
 
-  handleChange4(event) {
+  const handleChange4 = (event) => {
     this.setState({ year: event.target.value });
-  }
+  };
 
-  handleSubmit(event) {
+  const handleSubmit = (event) => {
     const user = useUid()[0];
     console.log(user);
     // const user = 'user';
@@ -75,9 +58,9 @@ export default class Home extends Component {
         });
       });
     event.preventDefault();
-  }
+  };
 
-  handleSubmit2(event) {
+  const handleSubmit2 = (event) => {
     const user = 'user';
     const { gender } = this.state;
     const { year } = this.state;
@@ -116,103 +99,101 @@ export default class Home extends Component {
 
     // alert(`An essay was submitted: ${gender} and ${year} and ${symptoms}`);
     event.preventDefault();
-  }
+  };
 
-  render() {
-    return (
-      <center>
-        <div className="container">
-          <Tabs
-            activeTab={{
-              id: 'tab1',
-            }}
-          >
-            <Tabs.Tab id="tab1" title="Diagnosis">
-              <div className="pt-3">
-                <form onSubmit={this.handleSubmit2} className="form">
-                  <input
-                    type="text"
-                    name="gender"
-                    placeholder="Gender"
-                    className="form-control mb-2"
-                    value={this.state.gender}
-                    onChange={this.handleChange3}
-                  />
-                  <input
-                    type="text"
-                    name="year"
-                    placeholder="Year of Birth"
-                    className="form-control mb-3"
-                    value={this.state.year}
-                    onChange={this.handleChange4}
-                  />
-                  <textarea
-                    value={this.state.symptoms}
-                    onChange={this.handleChange5}
-                    placeholder="Write your symptoms, separated by commas..."
-                    className="form-control pb-5 mb-3"
-                  />
-                  <input
-                    type="text"
-                    placeholder="How long ago did these symptoms first appear?"
-                    className="form-control mb-3"
-                  />
-                  <input
-                    type="text"
-                    placeholder="What is the duration of the symptoms (constant, few minutes in the day, etc.)?"
-                    className="form-control mb-3"
-                  />
-                  <input
-                    type="text"
-                    placeholder="How do the symptoms affect daily activities?"
-                    className="form-control mb-3"
-                  />
-                  <input
-                    type="text"
-                    placeholder="What medical conditions were/are you diagnosed with?"
-                    className="form-control mb-3"
-                  />
-                  <input
-                    type="text"
-                    placeholder="What past treatments have you tried (medications, hot pack, ice, etc.) and how did they affect you?"
-                    className="form-control mb-3"
-                  />
-                  <input
-                    type="submit"
-                    className="btn btn-primary"
-                    value="Submit"
-                  />
-                </form>
-              </div>
-            </Tabs.Tab>
-            <Tabs.Tab id="tab2" title="Custom">
-              <div>
-                <form onSubmit={this.handleSubmit}>
-                  <input
-                    type="text"
-                    name="name"
-                    placeholder="Title"
-                    className="form-control mt-3 mb-2"
-                    value={this.state.text}
-                    onChange={this.handleChange2}
-                  />
-                  <textarea
-                    value={this.state.value}
-                    className="form-control mb-3 pb-5"
-                    placeholder="Write your post here..."
-                    onChange={this.handleChange}
-                  />
-                  <input
-                    type="submit"
-                    className="btn btn-primary"
-                    value="Submit"
-                  />
-                </form>
-              </div>
-            </Tabs.Tab>
-          </Tabs>
-        </div>
-      </center>
-    );
-  }
-}
+  return (
+    <center>
+      <div className="container">
+        <Tabs
+          activeTab={{
+            id: 'tab1',
+          }}
+        >
+          <Tabs.Tab id="tab1" title="Diagnosis">
+            <div className="pt-3">
+              <form onSubmit={this.handleSubmit2} className="form">
+                <input
+                  type="text"
+                  name="gender"
+                  placeholder="Gender"
+                  className="mb-2 form-control"
+                  value={this.state.gender}
+                  onChange={this.handleChange3}
+                />
+                <input
+                  type="text"
+                  name="year"
+                  placeholder="Year of Birth"
+                  className="mb-3 form-control"
+                  value={this.state.year}
+                  onChange={this.handleChange4}
+                />
+                <textarea
+                  value={this.state.symptoms}
+                  onChange={this.handleChange5}
+                  placeholder="Write your symptoms, separated by commas..."
+                  className="pb-5 mb-3 form-control"
+                />
+                <input
+                  type="text"
+                  placeholder="How long ago did these symptoms first appear?"
+                  className="mb-3 form-control"
+                />
+                <input
+                  type="text"
+                  placeholder="What is the duration of the symptoms (constant, few minutes in the day, etc.)?"
+                  className="mb-3 form-control"
+                />
+                <input
+                  type="text"
+                  placeholder="How do the symptoms affect daily activities?"
+                  className="mb-3 form-control"
+                />
+                <input
+                  type="text"
+                  placeholder="What medical conditions were/are you diagnosed with?"
+                  className="mb-3 form-control"
+                />
+                <input
+                  type="text"
+                  placeholder="What past treatments have you tried (medications, hot pack, ice, etc.) and how did they affect you?"
+                  className="mb-3 form-control"
+                />
+                <input
+                  type="submit"
+                  className="btn btn-primary"
+                  value="Submit"
+                />
+              </form>
+            </div>
+          </Tabs.Tab>
+          <Tabs.Tab id="tab2" title="Custom">
+            <div>
+              <form onSubmit={this.handleSubmit}>
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Title"
+                  className="mt-3 mb-2 form-control"
+                  value={this.state.text}
+                  onChange={this.handleChange2}
+                />
+                <textarea
+                  value={this.state.value}
+                  className="pb-5 mb-3 form-control"
+                  placeholder="Write your post here..."
+                  onChange={this.handleChange}
+                />
+                <input
+                  type="submit"
+                  className="btn btn-primary"
+                  value="Submit"
+                />
+              </form>
+            </div>
+          </Tabs.Tab>
+        </Tabs>
+      </div>
+    </center>
+  );
+};
