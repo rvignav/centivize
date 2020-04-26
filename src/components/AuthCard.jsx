af82184ce81f0316d9995afa0cd2b948170c11e1
masonwang025 <mason.wang0025@gmail.com>
2020-04-25 21:08:06 -0700

    auth

--- src/components/AuthCard.jsx
+++ src/components/AuthCard.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
-import styled from 'styled-components';
 
 import {
   auth,
@@ -24,27 +23,19 @@ const uiConfig = {
   ],
 };
 
-const AuthCardStyles = styled.div`
-  box-shadow: 0 5px 5px 0 rgba(154, 160, 185, 0.05),
-    0 5px 30px 0 rgba(166, 173, 201, 0.22);
-  width: 300px;
-  margin: 0 auto;
-  padding: 20px 0;
-  border-radius: 8px;
-`;
-
-const SignInTitle = styled.h2`
-  font-weight: normal;
-  font-size: 14px;
-  padding: 0 0 0 40px;
-  margin-top: 15px;
-`;
-
 const AuthCard = () => (
-  <AuthCardStyles>
-    <SignInTitle>Sign in with one of the following:</SignInTitle>
-    <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />
-  </AuthCardStyles>
+  <div className="container">
+    <div className="card shadow-sm">
+      <div className="card-header">
+        <h5 className=" text-center mb-1">
+          Sign in with one of the following:{' '}
+        </h5>
+      </div>
+      <div className="card-body">
+        <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />
+      </div>
+    </div>
+  </div>
 );
 
 export default AuthCard;
