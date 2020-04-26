41c5715f405c31a99d7b0eb4d1562e61cb702496
rvignav <rvignav@gmail.com>
2020-04-26 12:51:42 -0700

    Update demo link

--- src/components/posts/ListCard.jsx
+++ src/components/posts/ListCard.jsx
@@ -4,32 +4,31 @@ import CardBody from './CardBody';
 
 export class ListCard extends Component {
   toggleCollapsed = (id) => {
-    let button = document.getElementById('collapseButton' + id);
+    const button = document.getElementById(`collapseButton${id}`);
     button.innerText =
       button.innerText === 'Learn more' ? 'Show less' : 'Learn more';
-    let info = document.getElementById('collapseInfo' + id);
+    const info = document.getElementById(`collapseInfo${id}`);
     info.style.display = info.style.display === 'block' ? 'none' : 'block';
   };
 
   changeVote = (id, amount) => {
     // get buttons
-    let upvote = document.getElementById('vote1' + id);
-    let downvote = document.getElementById('vote0' + id);
+    const upvote = document.getElementById(`vote1${id}`);
+    const downvote = document.getElementById(`vote0${id}`);
     // if one of these is green, there has been a vote so just return
     if (
       upvote.className.includes('btn-success') ||
       downvote.className.includes('btn-success')
     ) {
-      return;
     } else {
       // otherwise set the correct one to green
       if (amount === 1) {
         upvote.className = 'btn-sm btn btn-success';
-        console.log(id + ' has been UPVOTED');
+        console.log(`${id} has been UPVOTED`);
         // FIREBASE CODE HERE for upvoting id
       } else {
         downvote.className = 'btn-sm btn btn-success';
-        console.log(id + ' has been DOWNVOTED');
+        console.log(`${id} has been DOWNVOTED`);
         // FIREBASE CODE HERE for downvoting id
       }
     }
@@ -38,19 +37,18 @@ export class ListCard extends Component {
   // CURRENTLY:
   // CardHeader holds Title, Author, and Date, Upvote/Downvote buttons
   // CardBody holds the message, help button, and detailed paragraph (expandable on 'Learn more')
-  turnGreen = (id) => {
-    card = document.getElementById('card-' + id);
-    card.style.backgroundColor = 'green';
-  };
+  //   let turnGreen = (id) => {
+  //   let card = document.getElementById('card-' + id);
+  //   card.style.backgroundColor = 'green';
+  // };
 
   render() {
     return (
       <div>
         <div
-          id={'card-' + this.props.post.id}
+          id={`card-${this.props.post.id}`}
           key={this.props.post.id}
           className="card mb-2 m-0 w-100 post"
-          onClick={() => turnGreen(this.props.id)}
         >
           <CardHeader
             id={this.props.post.id}
--- src/pages/Demo.jsx
+++ src/pages/Demo.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 
 const Demo = () => {
   useEffect(() => {
-    window.location.assign('http://github.com');
+    window.location.assign('https://youtu.be/52Xu6-JoDy0');
   }, []);
 
   return <div />;
