1c438089aeb1579c8e067a1e6355efe14e2e6ad1
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2020-04-26 01:09:16 -0700

    Fix missing parentheses

--- functions/seed.js
+++ functions/seed.js
@@ -1,5 +1,32 @@
+const admin = require('firebase-admin');
+admin.initializeApp();
 
+const faker = require('faker');
 
-[...Array(10)].forEach(() => {
+const db = admin.firestore();
 
-})
+function coinFlip() {
+  return (Math.floor(Math.random() * 2) == 0);
+}
+
+function getRandomInRange(from, to, fixed) {
+  return (Math.random() * (to - from) + from).toFixed(fixed) * 1;
+  // .toFixed() returns string, so ' * 1' is a trick to convert to number
+}
+
+const fakeIt = () => {
+    return db.collection('posts').add({
+        type: coinFlip() ? "diagnosis" : "custom",
+        title: faker.lorem.sentence(),
+        message: faker.lorem.paragraphs(),
+        _geoloc: {
+          "lat":getRandomInRange(-180, 180, 3), 
+          "lng":getRandomInRange(-180, 180, 3)
+        },
+        author: {
+          
+        }
+    });
+}
+
+[...Array(10)].forEach(fakeIt)
--- src/components/posts/Post.jsx
+++ src/components/posts/Post.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
 import { Tabs } from '@yazanaabed/react-tabs';
 import { confirmAlert } from 'react-confirm-alert'; // Import
@@ -23,7 +23,7 @@ const Post = () => {
     navigator.geolocation.getCurrentPosition(function (position) {
       latitude = position.coords.latitude;
       longitude = position.coords.longitude;
-      location = { "lat": latitude, "lng": longitude };
+      location = { lat: latitude, lng: longitude };
       db.doc(`users/${uid}`)
         .set({
           location,
@@ -115,7 +115,7 @@ const Post = () => {
             .add({
               title: 'Need treatment help',
               message: str,
-              type: 'custom',
+              type: 'diagnosis',
               _geoloc: location,
               timePosted: new Date(),
               address,
@@ -132,105 +132,106 @@ const Post = () => {
                 ],
               });
             });
-        }
+        },
       );
-      event.preventDefault();
-    }
-  return (
-      <center>
-        <div className="container">
-          <Tabs
-            activeTab={{
-              id: 'tab1',
-            }}
-          >
-            <Tabs.Tab id="tab1" title="Diagnosis">
-              <div className="pt-3">
-                <form onSubmit={handleSubmit2} className="form">
-                  <input
-                    type="text"
-                    name="gender"
-                    placeholder="Gender"
-                    className="mb-2 form-control"
-                    value={gender}
-                    onChange={handleChange3}
-                  />
-                  <input
-                    type="text"
-                    name="year"
-                    placeholder="Year of Birth"
-                    className="mb-3 form-control"
-                    value={year}
-                    onChange={handleChange4}
-                  />
-                  <textarea
-                    value={symptoms}
-                    onChange={handleChange5}
-                    placeholder="Write your symptoms, separated by commas..."
-                    className="pb-5 mb-3 form-control"
-                  />
-                  <input
-                    type="text"
-                    placeholder="How long ago did these symptoms first appear?"
-                    className="mb-3 form-control"
-                  />
-                  <input
-                    type="text"
-                    placeholder="What is the duration of the symptoms (constant, few minutes in the day, etc.)?"
-                    className="mb-3 form-control"
-                  />
-                  <input
-                    type="text"
-                    placeholder="How do the symptoms affect daily activities?"
-                    className="mb-3 form-control"
-                  />
-                  <input
-                    type="text"
-                    placeholder="What medical conditions were/are you diagnosed with?"
-                    className="mb-3 form-control"
-                  />
-                  <input
-                    type="text"
-                    placeholder="What past treatments have you tried (medications, hot pack, ice, etc.) and how did they affect you?"
-                    className="mb-3 form-control"
-                  />
-                  <input
-                    type="submit"
-                    className="btn btn-primary"
-                    value="Submit"
-                  />
-                </form>
-              </div>
-            </Tabs.Tab>
-            <Tabs.Tab id="tab2" title="Custom">
-              <div>
-                <form onSubmit={handleSubmit}>
-                  <input
-                    type="text"
-                    name="name"
-                    placeholder="Title"
-                    className="mt-3 mb-2 form-control"
-                    value={text}
-                    onChange={handleChange2}
-                  />
-                  <textarea
-                    value={value}
-                    className="pb-5 mb-3 form-control"
-                    placeholder="Write your post here..."
-                    onChange={handleChange}
-                  />
-                  <input
-                    type="submit"
-                    className="btn btn-primary"
-                    value="Submit"
-                  />
-                </form>
-              </div>
-            </Tabs.Tab>
-          </Tabs>
-        </div>
-      </center>
-    );
+    });
+    event.preventDefault();
   };
+  return (
+    <center>
+      <div className="container">
+        <Tabs
+          activeTab={{
+            id: 'tab1',
+          }}
+        >
+          <Tabs.Tab id="tab1" title="Diagnosis">
+            <div className="pt-3">
+              <form onSubmit={handleSubmit2} className="form">
+                <input
+                  type="text"
+                  name="gender"
+                  placeholder="Gender"
+                  className="mb-2 form-control"
+                  value={gender}
+                  onChange={handleChange3}
+                />
+                <input
+                  type="text"
+                  name="year"
+                  placeholder="Year of Birth"
+                  className="mb-3 form-control"
+                  value={year}
+                  onChange={handleChange4}
+                />
+                <textarea
+                  value={symptoms}
+                  onChange={handleChange5}
+                  placeholder="Write your symptoms, separated by commas..."
+                  className="pb-5 mb-3 form-control"
+                />
+                <input
+                  type="text"
+                  placeholder="How long ago did these symptoms first appear?"
+                  className="mb-3 form-control"
+                />
+                <input
+                  type="text"
+                  placeholder="What is the duration of the symptoms (constant, few minutes in the day, etc.)?"
+                  className="mb-3 form-control"
+                />
+                <input
+                  type="text"
+                  placeholder="How do the symptoms affect daily activities?"
+                  className="mb-3 form-control"
+                />
+                <input
+                  type="text"
+                  placeholder="What medical conditions were/are you diagnosed with?"
+                  className="mb-3 form-control"
+                />
+                <input
+                  type="text"
+                  placeholder="What past treatments have you tried (medications, hot pack, ice, etc.) and how did they affect you?"
+                  className="mb-3 form-control"
+                />
+                <input
+                  type="submit"
+                  className="btn btn-primary"
+                  value="Submit"
+                />
+              </form>
+            </div>
+          </Tabs.Tab>
+          <Tabs.Tab id="tab2" title="Custom">
+            <div>
+              <form onSubmit={handleSubmit}>
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Title"
+                  className="mt-3 mb-2 form-control"
+                  value={text}
+                  onChange={handleChange2}
+                />
+                <textarea
+                  value={value}
+                  className="pb-5 mb-3 form-control"
+                  placeholder="Write your post here..."
+                  onChange={handleChange}
+                />
+                <input
+                  type="submit"
+                  className="btn btn-primary"
+                  value="Submit"
+                />
+              </form>
+            </div>
+          </Tabs.Tab>
+        </Tabs>
+      </div>
+    </center>
+  );
+};
 
-  export default Post;
+export default Post;
