dce7574c28f4649668ef02565d012b851f3faefc
rvignav <rvignav@gmail.com>
2020-04-26 00:59:27 -0700

    edit Post.jsx

--- src/components/posts/Post.jsx
+++ src/components/posts/Post.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
 import { Tabs } from '@yazanaabed/react-tabs';
 import { confirmAlert } from 'react-confirm-alert'; // Import
@@ -6,15 +6,33 @@ import { db } from '../../firebase/firebase.utils.js';
 import diagnose from '../../diagnosis.js';
 import { useUid } from '../../hooks/auth.js';
 import 'react-confirm-alert/src/react-confirm-alert.css';
+import Geocode from 'react-geocode';
 
 const Post = () => {
-  const user = useUid()[0];
-  console.log(user);
+  const uid = useUid()[0];
+  console.log(uid);
   const [value, setValue] = useState('');
   const [text, setText] = useState('');
   const [gender, setGender] = useState('');
   const [year, setYear] = useState('');
   const [symptoms, setSymptoms] = useState('');
+  let location;
+  let latitude;
+  let longitude;
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      latitude = position.coords.latitude;
+      longitude = position.coords.longitude;
+      location = { "lat": latitude, "lng": longitude };
+      db.doc(`users/${uid}`)
+        .set({
+          location,
+        })
+        .then(function () {
+          console.log('DONE');
+        });
+    });
+  }, []);
 
   const handleChange = (event) => {
     setValue(event.target.value);
@@ -39,23 +57,37 @@ const Post = () => {
   const handleSubmit = (event) => {
     const title = text;
     const post = value;
-    db.collection(user)
-      .add({
-        title,
-        post,
-      })
-      .then(function () {
-        confirmAlert({
-          title: 'Thanks for submitting.',
-          message: 'Your response has been recorded.',
-          buttons: [
-            {
-              label: 'OK',
-              onClick: () => window.location.reload(),
-            },
-          ],
-        });
-      });
+    let address;
+    Geocode.setApiKey('AIzaSyARn00rdknaP7N9Qjzhv8duDJo1Dxkv2ZA');
+    Geocode.fromLatLng(latitude.toString(), longitude.toString()).then(
+      (response) => {
+        address = response.results[0].formatted_address;
+        db.collection(uid)
+          .add({
+            title,
+            message: post,
+            type: 'custom',
+            _geoloc: location,
+            timePosted: new Date(),
+            address,
+          })
+          .then(function () {
+            confirmAlert({
+              title: 'Thanks for submitting.',
+              message: 'Your response has been recorded.',
+              buttons: [
+                {
+                  label: 'OK',
+                  onClick: () => window.location.reload(),
+                },
+              ],
+            });
+          });
+      },
+      (error) => {
+        console.error(error);
+      },
+    );
     event.preventDefault();
   };
 
@@ -74,122 +106,131 @@ const Post = () => {
         str += `${issues[i]}: ${treatments[i]} \n`;
       }
       console.log(`str: ${str}`);
-      db.collection(user)
-        .add({
-          title: 'Need treatment help',
-          post: str,
-        })
-        .then(function () {
-          confirmAlert({
-            title: 'Thanks for submitting.',
-            message: 'Your response has been recorded.',
-            buttons: [
-              {
-                label: 'OK',
-                onClick: () => window.location.reload(),
-              },
-            ],
-          });
-        });
-    });
-    event.preventDefault();
+      let address;
+      Geocode.setApiKey('AIzaSyARn00rdknaP7N9Qjzhv8duDJo1Dxkv2ZA');
+      Geocode.fromLatLng(latitude.toString(), longitude.toString()).then(
+        (response) => {
+          address = response.results[0].formatted_address;
+          db.collection(uid)
+            .add({
+              title: 'Need treatment help',
+              message: str,
+              type: 'custom',
+              _geoloc: location,
+              timePosted: new Date(),
+              address,
+            })
+            .then(function () {
+              confirmAlert({
+                title: 'Thanks for submitting.',
+                message: 'Your response has been recorded.',
+                buttons: [
+                  {
+                    label: 'OK',
+                    onClick: () => window.location.reload(),
+                  },
+                ],
+              });
+            });
+        }
+      );
+      event.preventDefault();
+    }
+  return (
+      <center>
+        <div className="container">
+          <Tabs
+            activeTab={{
+              id: 'tab1',
+            }}
+          >
+            <Tabs.Tab id="tab1" title="Diagnosis">
+              <div className="pt-3">
+                <form onSubmit={handleSubmit2} className="form">
+                  <input
+                    type="text"
+                    name="gender"
+                    placeholder="Gender"
+                    className="mb-2 form-control"
+                    value={gender}
+                    onChange={handleChange3}
+                  />
+                  <input
+                    type="text"
+                    name="year"
+                    placeholder="Year of Birth"
+                    className="mb-3 form-control"
+                    value={year}
+                    onChange={handleChange4}
+                  />
+                  <textarea
+                    value={symptoms}
+                    onChange={handleChange5}
+                    placeholder="Write your symptoms, separated by commas..."
+                    className="pb-5 mb-3 form-control"
+                  />
+                  <input
+                    type="text"
+                    placeholder="How long ago did these symptoms first appear?"
+                    className="mb-3 form-control"
+                  />
+                  <input
+                    type="text"
+                    placeholder="What is the duration of the symptoms (constant, few minutes in the day, etc.)?"
+                    className="mb-3 form-control"
+                  />
+                  <input
+                    type="text"
+                    placeholder="How do the symptoms affect daily activities?"
+                    className="mb-3 form-control"
+                  />
+                  <input
+                    type="text"
+                    placeholder="What medical conditions were/are you diagnosed with?"
+                    className="mb-3 form-control"
+                  />
+                  <input
+                    type="text"
+                    placeholder="What past treatments have you tried (medications, hot pack, ice, etc.) and how did they affect you?"
+                    className="mb-3 form-control"
+                  />
+                  <input
+                    type="submit"
+                    className="btn btn-primary"
+                    value="Submit"
+                  />
+                </form>
+              </div>
+            </Tabs.Tab>
+            <Tabs.Tab id="tab2" title="Custom">
+              <div>
+                <form onSubmit={handleSubmit}>
+                  <input
+                    type="text"
+                    name="name"
+                    placeholder="Title"
+                    className="mt-3 mb-2 form-control"
+                    value={text}
+                    onChange={handleChange2}
+                  />
+                  <textarea
+                    value={value}
+                    className="pb-5 mb-3 form-control"
+                    placeholder="Write your post here..."
+                    onChange={handleChange}
+                  />
+                  <input
+                    type="submit"
+                    className="btn btn-primary"
+                    value="Submit"
+                  />
+                </form>
+              </div>
+            </Tabs.Tab>
+          </Tabs>
+        </div>
+      </center>
+    );
   };
 
-  return (
-    <center>
-      <div className="container">
-        <Tabs
-          activeTab={{
-            id: 'tab1',
-          }}
-        >
-          <Tabs.Tab id="tab1" title="Diagnosis">
-            <div className="pt-3">
-              <form onSubmit={handleSubmit2} className="form">
-                <input
-                  type="text"
-                  name="gender"
-                  placeholder="Gender"
-                  className="mb-2 form-control"
-                  value={gender}
-                  onChange={handleChange3}
-                />
-                <input
-                  type="text"
-                  name="year"
-                  placeholder="Year of Birth"
-                  className="mb-3 form-control"
-                  value={year}
-                  onChange={handleChange4}
-                />
-                <textarea
-                  value={symptoms}
-                  onChange={handleChange5}
-                  placeholder="Write your symptoms, separated by commas..."
-                  className="pb-5 mb-3 form-control"
-                />
-                <input
-                  type="text"
-                  placeholder="How long ago did these symptoms first appear?"
-                  className="mb-3 form-control"
-                />
-                <input
-                  type="text"
-                  placeholder="What is the duration of the symptoms (constant, few minutes in the day, etc.)?"
-                  className="mb-3 form-control"
-                />
-                <input
-                  type="text"
-                  placeholder="How do the symptoms affect daily activities?"
-                  className="mb-3 form-control"
-                />
-                <input
-                  type="text"
-                  placeholder="What medical conditions were/are you diagnosed with?"
-                  className="mb-3 form-control"
-                />
-                <input
-                  type="text"
-                  placeholder="What past treatments have you tried (medications, hot pack, ice, etc.) and how did they affect you?"
-                  className="mb-3 form-control"
-                />
-                <input
-                  type="submit"
-                  className="btn btn-primary"
-                  value="Submit"
-                />
-              </form>
-            </div>
-          </Tabs.Tab>
-          <Tabs.Tab id="tab2" title="Custom">
-            <div>
-              <form onSubmit={handleSubmit}>
-                <input
-                  type="text"
-                  name="name"
-                  placeholder="Title"
-                  className="mt-3 mb-2 form-control"
-                  value={text}
-                  onChange={handleChange2}
-                />
-                <textarea
-                  value={value}
-                  className="pb-5 mb-3 form-control"
-                  placeholder="Write your post here..."
-                  onChange={handleChange}
-                />
-                <input
-                  type="submit"
-                  className="btn btn-primary"
-                  value="Submit"
-                />
-              </form>
-            </div>
-          </Tabs.Tab>
-        </Tabs>
-      </div>
-    </center>
-  );
-};
-
-export default Post;
+  export default Post;
