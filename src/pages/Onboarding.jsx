515eefd7de5251dea949dc2493b6455c0b28f566
masonwang025 <mason.wang0025@gmail.com>
2020-04-25 21:45:09 -0700

    reset

--- src/pages/Onboarding.jsx
+++ src/pages/Onboarding.jsx
@@ -3,22 +3,14 @@ import { Redirect } from 'react-router-dom';
 
 import { useFirstTime } from '../hooks/auth';
 
-import { Onboarding, Step } from '@reactive-labs/onboarding';
-
-const OnboardingSwiper = () => {
+const Onboarding = () => {
   const [firstTime, loadingFirstTime] = useFirstTime();
 
   if (!loadingFirstTime && !firstTime) {
     return <Redirect to="/app" />;
   }
 
-  return (
-    <Onboarding>
-      <Step name="welcome">
-        
-      </Step>
-    </Onboarding>
-  );
+  return <div />;
 };
 
-export default OnboardingSwiper;
+export default Onboarding;
