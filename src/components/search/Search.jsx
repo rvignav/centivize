a6109997abde1cbabcb4beec03c2b439b2ec98e4
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2020-04-26 07:17:40 -0700

    make search.jsx functional component

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import { Tabs } from '@yazanaabed/react-tabs';
 
 import SearchInfo from './SearchInfo';
@@ -7,71 +7,65 @@ import PostFeed from '../posts/PostFeed';
 import dummyData from '../../dummyposts';
 import distance from '../../distance.js';
 
-export class Search extends Component {
-  constructor() {
-    super();
-    const posts = this.loadPosts();
-    this.state = {
-      sort: 'date',
-      filterText: '',
-      posts,
-    };
-  }
+const Search = () => {
+  const [sortMethod, setSortMethod] = useState('date');
+  const [searchQuery, setSearchQuery] = useState('');
+  const [posts, setPosts] = useState([]);
 
-  componentDidMount() {
-    console.log(
-      `FINAL DIST: ${distance(40.78382, -73.97536, 40.7039, -73.986909)}`,
-    );
-  }
+//   const []
 
-  loadPosts = () => {
+  useEffect(
+    () =>
+      console.log(
+        `FINAL DIST: ${distance(40.78382, -73.97536, 40.7039, -73.986909)}`,
+      ),
+    [],
+  );
+
+  const loadPosts = () => {
     return dummyData;
   };
 
-  toggleSetting = (setting) => {
-    this.setState({ sort: setting });
+  const toggleSetting = (setting) => {
+    setSortMethod(setting);
   };
 
-  filterTextUpdate = (text) => {
-    this.setState({ filterText: text });
-    this.sortPosts();
-  };
-
-  sortPosts = () => {
+  const sortPosts = () => {
     console.log('filtering posts');
   };
 
-  render() {
-    const { sort, posts } = this.state;
+  const filterTextUpdate = (text) => {
+    setSearchQuery(text);
+    sortPosts();
+  };
 
-    return (
-      <div>
-        <div className="container-fluid search-upper">
-          <SearchInfo
-            sort={sort}
-            toggleSetting={this.toggleSetting}
-            filterTextUpdate={this.filterTextUpdate}
-          />
-        </div>
-        <div className="container search-lower">
-          <Tabs
-            activeTab={{
-              id: 'list-tab',
-            }}
-          >
-            <Tabs.Tab id="list-tab" title="List">
-              <div className="pt-3">
-                <PostFeed posts={posts} />
-              </div>
-            </Tabs.Tab>
-            <Tabs.Tab id="map-tab" title="Map">
-              <div />
-            </Tabs.Tab>
-          </Tabs>
-        </div>
+  return (
+    <div>
+      <div className="container-fluid search-upper">
+        <SearchInfo
+          sort={sortMethod}
+          toggleSetting={toggleSetting}
+          filterTextUpdate={filterTextUpdate}
+        />
       </div>
-    );
-  }
-}
+      <div className="container search-lower">
+        <Tabs
+          activeTab={{
+            id: 'list-tab',
+          }}
+        >
+          <Tabs.Tab id="list-tab" title="List">
+            <div className="pt-3">
+              <PostFeed posts={posts} />
+            </div>
+          </Tabs.Tab>
+          <Tabs.Tab id="map-tab" title="Map">
+            <div />
+          </Tabs.Tab>
+        </Tabs>
+      </div>
+    </div>
+  );
+};
 
 export default Search;
