f09b5fc9aba063b2a9c91bafad08c2b5f7cf6326
masonwang025 <mason.wang0025@gmail.com>
2020-04-25 21:13:23 -0700

    react

--- src/components/WelcomeHeader.jsx
+++ src/components/WelcomeHeader.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const WelcomeHeader = () => (
-  <header class="container text-center pt-3 pb-3">
-    <h1 class="text-primary">Communify</h1>
+  <header className="container text-center pt-3 pb-3">
+    <h1 className="text-primary">Communify</h1>
     <h3>Welcome to Communify</h3>
   </header>
 );
