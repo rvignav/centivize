91d034592ed0218135493a80a2ac8a9a28357a35
MasonWang025 <mason.wang0025@gmail.com>
2020-04-26 09:42:27 -0700

    centivize it

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export class Header extends Component {
     render() {
         return (
             <header className="fixed-top logo-header text-center">
-                <img src={logo} className="img-fluid col-md-2 col-sm-3 col-4 col-lg-1" />
+                <img src={logo} className="img-fluid col-md-2 col-sm-3 col-4 col-lg-1 pb-xl-2 pt-xl-2" />
             </header>
         )
     }
