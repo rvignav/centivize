f3d300beb38de745f29343fdf725e1ad10a29d1b
Ammesatyajit <ammesatyajit@gmail.com>
2020-04-26 04:06:34 -0700

    geofence

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,36 +1,41 @@
 import React, { Component } from 'react';
 import SignOutButton from './auth/SignOutButton';
+import Radar from 'radar-sdk-js';
+import { useUid } from '../hooks/auth.js';
 
 import Header from './Header';
 
 import profilePicture from '../assets/profile-test.jpg';
 
-export class Home extends Component {
-  render() {
-    return (
-      <div>
-        <Header />
+Radar.initialize('prj_live_pk_9960fa9015ec1c672178a43fa62142afc16b6aed');
 
-        <div className="container text-center home-main profile">
+const Profile = () => {
+  const uid = useUid()[0];
+  console.log(uid);
+  Radar.setUserId(uid);
+  return (
+    <div>
+      <Header />
+
+      <div className="container text-center home-main profile">
         <div className="row justify-content-center">
-        <div className="col">
-          <img
+          <div className="col">
+            <img
               className="img-fluid round"
               src={profilePicture}
               alt="profile picture"
             />
-            
+
           </div>
         </div>
-          <h1>John Doe</h1>
-          <p className="email">johndoe1@gmail.com</p>
-          <p>Primarily volunteers/helps others. Volunteering interests/skills: food delivery.</p>
-          <p className="balance">Current balance: <span className="value">$25.00</span></p>
-          <SignOutButton />
-        </div>
+        <h1>John Doe</h1>
+        <p className="email">johndoe1@gmail.com</p>
+        <p>Primarily volunteers/helps others. Volunteering interests/skills: food delivery.</p>
+        <p className="balance">Current balance: <span className="value">$25.00</span></p>
+        <SignOutButton />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-export default Home;
+export default Profile;
--- src/geofence.js
+++ src/geofence.js
@@ -1,12 +1,21 @@
 import Radar from 'radar-sdk-js';
+import { db } from './firebase/firebase.utils.js';
 Radar.initialize('prj_live_pk_9960fa9015ec1c672178a43fa62142afc16b6aed');
 
-const geofence = async (lat, lng, radius, desc) => {
+const geofence = async (lat, lng, radius, desc, uid) => {
     fetch("https://api.radar.io/v1/geofences?description="+desc+"&type=circle&coordinates=["+lng+","+lat+"]&radius="+radius, {
-    headers: {
-        Authorization: "prj_live_pk_9960fa9015ec1c672178a43fa62142afc16b6aed"
-    }
-    })
+        headers: {
+            Authorization: "prj_live_pk_9960fa9015ec1c672178a43fa62142afc16b6aed"
+        },
+        method: "PUT"
+    }).then(() => {
+        const tag = desc.split('#')[0].trim();
+        const externalId = desc.split('#')[1];
+        db.collection(uid).add({
+            geofence: {tag, externalId}
+          });
+    });
+    
 }
 
-export default geofence
+export default geofence;
