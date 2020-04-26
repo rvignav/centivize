2ec19195b0f2d8ab8cdd2d1e64f8738ed0657ba2
MasonWang025 <mason.wang0025@gmail.com>
2020-04-26 08:42:20 -0700

    thing

--- src/components/onboarding/checkbox.component.js
+++ src/components/onboarding/checkbox.component.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
 export default function checkbox() {
+  let toggleDoctorShow = () => {
+    let div = document.getElementById('doctorupload')
+    div.style.display = div.style.display == "block" ? "none" : "block"
+  }
   return (
-    <div className="lead">
+    <div className="lead" id="checkboxarea">
       <div className="form-check pt-2">
         <input
           className="form-check-input"
@@ -14,7 +18,7 @@ export default function checkbox() {
           Asking for help
         </label>
       </div>
-      <div className="form-check pt-1 pb-4">
+      <div className="form-check pt-1">
         <input
           className="form-check-input"
           type="checkbox"
@@ -24,6 +28,30 @@ export default function checkbox() {
         <label className="form-check-label" htmlFor="help">
           Helping others
         </label>
+      </div>
+      <div className="form-check pt-1 pb-4">
+        <input
+          className="form-check-input"
+          type="checkbox"
+          defaultValue
+          id="doctor"
+          onChange={()=> toggleDoctorShow()}
+        />
+        <label className="form-check-label font-weight-bold" htmlFor="help">
+          If you are a doctor, check this
+        </label>
+        <div className="form-group" id="doctorupload" style={{display: "none"}}>
+          <label for="exampleInputFile">File input</label>
+          <input
+            type="file"
+            className="form-control-file"
+            id="exampleInputFile"
+            aria-describedby="fileHelp"
+          />
+          <small id="fileHelp" className="form-text text-muted">
+            Please upload resume for review and approval.
+          </small>
+        </div>
       </div>
     </div>
   );
