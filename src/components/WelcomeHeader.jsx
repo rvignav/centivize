48d5677b159e989dabfae76ea24f9317938ee836
masonwang025 <mason.wang0025@gmail.com>
2020-04-25 21:12:06 -0700

    communify

--- src/components/WelcomeHeader.jsx
+++ src/components/WelcomeHeader.jsx
@@ -1,30 +1,10 @@
 import React from 'react';
-import styled from 'styled-components';
-
-import { ReactComponent as Logo } from '../assets/temp-logo.svg';
-
-const ResizedLogo = styled(Logo)`
-  width: 80px;
-  height: 80px;
-`;
-
-const HeaderLayout = styled.header`
-  margin: 20px 0;
-  text-align: center;
-  box-shadow: none;
-`;
-
-const Welcome = styled.h1`
-  margin: 15px 0;
-  font-weight: normal;
-  font-size: 22.5px;
-`;
 
 const WelcomeHeader = () => (
-  <HeaderLayout>
-    <ResizedLogo />
-    <Welcome>Welcome to Communify</Welcome>
-  </HeaderLayout>
+  <header class="container text-center pt-3 pb-3">
+    <h1 class="text-primary">Communify</h1>
+    <h3>Welcome to Communify</h3>
+  </header>
 );
 
 export default WelcomeHeader;
