49d60e9159d04eb7100a5f9c6352b52d7dd4398b
MasonWang025 <mason.wang0025@gmail.com>
2020-06-26 09:30:20 -0700

    npx create react app

--- src/components/onboarding/container.component.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Container = (props) => <div className="container mt-5">{props.children}</div>;
-
-export default Container;
--- src/components/onboarding/step-header.component.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const StepHeader = (props) => <h3>{props.children}</h3>;
-
-export default StepHeader;
